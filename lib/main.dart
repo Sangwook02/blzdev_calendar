cee1bad5d2d0e2ede36e31122041f5f9a16ecea4
chosw1002@naver.com <chosw1002@naver.com>
2023-01-03 14:34:23 +0900

    🐛 FIX: Change title of material app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'BlazingDevs Calendar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
